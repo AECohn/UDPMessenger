f56430f0589160748f8b3050b42d339c1b54e1cd
Aviv <acohn@dgitech.com>
2023-04-07 16:20:55 -0400

    init

---
+++ UDPServer/Messager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleUDP
+{
+    public class Messager
+    {
+        public class MessageReceivedEventArgs : EventArgs
+        {
+            public string Message { get; set; }
+        }
+
+        private bool Listening;
+
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+
+        public void Send(string Address, int Port, string Message)
+        {
+            UdpClient sender = new UdpClient();
+
+            sender.Connect(Address, Port);
+            sender.Send(System.Text.Encoding.UTF8.GetBytes(Message), Message.Length);
+            sender.Dispose();
+        }
+
+        public void StartListening()
+        {
+            Listening = true;
+            var ipEndPoint = new IPEndPoint(IPAddress.Any, 50000);
+
+            UdpClient listener = new UdpClient(ipEndPoint);
+
+
+            while (Listening)
+            {
+                var buffer = listener.ReceiveAsync();
+
+                // Convert the byte array to a string
+                string message = Encoding.UTF8.GetString(buffer.Result.Buffer);
+
+                // Display the message on the console
+                MessageReceived?.Invoke(this, new MessageReceivedEventArgs { Message = message });
+            }
+            
+            listener.Dispose();
+        }
+
+        private void StopListening()
+        {
+            Listening = false;
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleUDP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Messager messager = new Messager();
+            Console.WriteLine("type send to send or listen to listen");
+            messager.MessageReceived += (sender, eventArgs) => Console.WriteLine(eventArgs.Message);
+
+            if (Console.ReadLine() == "send")
+            {
+                messager.Send("192.168.1.20", 50000, Console.ReadLine());
+            }
+            else
+            {
+                messager.StartListening();
+            }
+
+            
+        }
+    }
+}
