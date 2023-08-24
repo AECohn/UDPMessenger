23ed3bb9a1334d508a9df660d1fea21d4d0d2f36
Aviv Cohn <acohn@dgitech.com>
2023-08-24 14:31:13 -0400

    Using a range of ports

--- SimpleUDP/Program.cs
+++ SimpleUDP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -8,49 +9,40 @@ namespace SimpleUDP
 {
     internal class Program
     {
-
-        static void OnUdpData(IAsyncResult result)
+        static async Task Main(string[] args)
         {
-            // this is what had been passed into BeginReceive as the second parameter:
-            UdpClient socket = result.AsyncState as UdpClient;
-            // points towards whoever had sent the message:
-            IPEndPoint source = new IPEndPoint(0, 0);
-            // get the actual message and fill out the source:
-            byte[] message = socket.EndReceive(result, ref source);
-            // do what you'd like with `message` here:
-            Console.WriteLine("Got " + message.Length + " bytes from " + source);
-            Console.WriteLine(Encoding.GetEncoding("ISO-8859-1").GetString(message));
-            // schedule the next receive operation once reading is done:
-            socket.BeginReceive(new AsyncCallback(OnUdpData), socket);
+            int startPort = 100; // Starting port of the range
+            int endPort = 70000;   // Ending port of the range
+
+            Console.WriteLine("Async UDP Port Listener");
+
+            var tasks = new List<Task>();
+
+            for (int port = startPort; port <= endPort; port++)
+            {
+                //Console.WriteLine($"Listening on port {port}");
+                tasks.Add(StartListeningAsync(port));
+            }
+
+            await Task.WhenAll(tasks);
         }
 
-        static void Main(string[] args)
+        static async Task StartListeningAsync(int port)
         {
-            // Specify the port to listen on
-            int port = 12345;
-
-            // Create a UDP client to listen for incoming messages
-            UdpClient udpClient = new UdpClient(12345);
-
-            Console.WriteLine("UDP listener started. Waiting for messages...");
+            UdpClient udpClient = new UdpClient(port);
 
             try
             {
                 while (true)
                 {
-                    // Receive a UDP message and the sender's information
-                    IPEndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, port);
-                    byte[] receivedBytes = udpClient.Receive(ref senderEndPoint);
-
-                    // Convert the received bytes to a string
-                    string receivedMessage = Encoding.UTF8.GetString(receivedBytes);
-
-                    Console.WriteLine($"Received message from {senderEndPoint}: {receivedMessage}");
+                    UdpReceiveResult result = await udpClient.ReceiveAsync();
+                    string message = Encoding.ASCII.GetString(result.Buffer);
+                    Console.WriteLine($"Received from {result.RemoteEndPoint} on port {port}: {message}");
                 }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.WriteLine($"An error occurred: {e.Message}");
             }
             finally
             {
